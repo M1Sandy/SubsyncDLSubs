626800fe3057d404ce60fb96416c689b23d44dd0
sandy <m1s4ndy@gmail.com>
2023-06-02 15:09:12 +0300

    code add

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # SubsyncDLSubs
-a scrypt to walk on your media directory to sync already downloaded subtitles with subsync.
+a script to walk on your media directory to sync already downloaded subtitles with subsync. Assuming you have fixed naming schema for both movies and their subtitles, like *.MKV for movie and *.en.srt
+
+<br />
+
+---
+## Usage:
+```
+python sync.py <PATH> <audio_lang> <subtitles_lang>
+```
+
+<br />
+
+### Example:
+```
+python sync.py "Z:\Movies" en ar
+```
+---
+
+
+### Packeges:
+pip install pymediainfo
+
+<br />
+
+--- 
+
+## Note: 
+- subsync should be installed and **subsync-cmd.exe** have to be added to windows's enviroment variable.
+    - to download: https://subsync.online/
+- tested on windows.
+- some arguments of sybsync are hardcoded in the script, feel free to tweak them as you like.
+<br />
+
+--- 
+## to-do 
+- add multithreading support to process large amount of media faster.
+- sync multiple subtitles with diff language to the same movie. (for now only one language per time)
---
+++ sync.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import sys
+from pymediainfo import MediaInfo
+
+def isVideo(file):
+    try:
+        fInfo = MediaInfo.parse(file)
+    except:
+        return False
+    for track in fInfo.tracks:
+        if track.track_type == "Video":
+            return True
+    return False
+
+def SubExist(file):
+    if os.path.exists(file):
+        return True
+    return False
+
+def GetSubFileName(dir, filename):
+    for file in dir:
+        if isVideo(file):
+            continue
+        if filename in file and file.endswith("srt"):
+            return file
+    return filename
+    
+
+if __name__ == "__main__":
+        if len(sys.argv) != 4:
+            print("[-] Usage: sync.py <PATH> <audio_lang> <subtitles_lang>")
+            exit()
+        rootdir = sys.argv[1]
+        sub_lang = str(sys.argv[2])
+        audio_lang = str(sys.argv[3])
+
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                if not file.endswith("srt") and isVideo(os.path.join(subdir,file)):
+                    tFileName = os.path.splitext(file)
+                    tmp = tFileName[0]
+                    relatedSub = GetSubFileName(files, tmp)
+                    if relatedSub == tmp:
+                        print(f"[-] No sub Found. [{tmp}]")
+                    tmp += "." + sub_lang + ".srt"
+                    if SubExist(os.path.join(subdir,tmp)):
+                        print("[*] Sub Found. Syncing . . .")
+                        cmd = ["subsync-cmd.exe",
+                                "sync",
+                                "--sub-lang",
+                                sub_lang,       # change this to your subtitle language
+                                "--ref-lang",
+                                audio_lang,        # change this to your movie audio language
+                                "--sub",
+                                os.path.join(subdir,tmp),
+                                "--ref" ,
+                                os.path.join(subdir,file), 
+                                "--out", os.path.join(subdir,tmp),
+                                "--window-size=10000",
+                                "--effort=1",
+                                "--overwrite"]
+                        proc = subprocess.Popen(cmd)
+                        proc.wait()
+                        print(f"[+] Synced.[{tmp}][{file}]")
+                    else:
+                        print(f"[-] Sub NOT Found. [{tmp}]")
+                        continue
+
+                    # print(f"[{os.path.join(subdir,tmp)}] [Video]")
+
+
